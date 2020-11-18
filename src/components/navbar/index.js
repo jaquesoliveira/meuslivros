0f03c8df2ab44b4829ae73470fa6ac67082fa2dd
jaques <jaques.oliveira@gmail.com>
2020-11-17 22:18:25 -0300

    alteração no menu retratil

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -9,14 +9,22 @@ function Navbar(){
                 <Link to="/" className="navbar-brand" href="#">
                     <i class="fas fa-book-reader"><span className="ml-2">Meus Livros</span></i>
                 </Link>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler" type="button" 
+                    data-toggle="collapse" 
+                    data-target="#navbarSupportedContent" 
+                    aria-controls="navbarSupportedContent" 
+                    aria-expanded="false" 
+                    aria-label="Toggle navigation">
+                    
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item ">
-                            <Link to="/" className="nav-link" href="#">
+                            <Link to="/" className="nav-link" href="#"
+                                data-target="#navbarSupportedContent"
+                                data-toggle="collapse">
                                 Início
                             </Link>
                         </li>
